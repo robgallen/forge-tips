648d3a79a96c6640f0700d101793fe05b5f8ac1d
Rob Allen <robgallen@gmail.com>
2021-05-30 10:19:04 +0100

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,8 +10,17 @@ module.exports = {
   parserOptions: {
     parser: 'babel-eslint'
   },
+  // add your custom rules here
   rules: {
+    // allow async-await
+    'generator-star-spacing': 'off',
+    // allow debugger during development
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // overrides
+    'vue/v-bind-style': ['warn', 'longform'],
+    'vue/v-on-style': ['warn', 'longform'],
+    'vue/max-attributes-per-line': 'off',
+    'semi': ['error', 'always']
   }
 }
